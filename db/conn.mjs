d101c203d8a51c3eb8020bd71b0f9f43b4eba747
bielsesa <bielsesa@gmail.com>
2024-11-09 00:29:56 +0100

    Add Postman collection and improve MongoDB connection.
    Added a new Postman collection for KONE Junction 2024 with endpoints for building management and authentication. Updated the MongoDB connection logic to use additional environment variables for database name and connection parameters.

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
 
-const connectionString = process.env.ATLAS_URI || "";
+const baseConnectionString = process.env.ATLAS_URI || "";
+const dbName = process.env.DB_NAME || "";
+const connectionParams = process.env.CONNECTION_PARAMS || "";
 
-if (!connectionString) {
-    console.error("ATLAS_URI is not defined");
+if (!baseConnectionString || !dbName || !connectionParams) {
+    console.error("ATLAS_URI or DB_NAME or CONNECTION_PARAMS is not defined");
 } else {
+    const connectionString = `${baseConnectionString}/${dbName}?${connectionParams}`;
     console.log(`Using connection string: ${connectionString}`);
-}
 
-mongoose.connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.error("Error connecting to MongoDB:", err));
+    mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => console.log("MongoDB Connected"))
+        .catch(err => console.error("Error connecting to MongoDB:", err));
+}
 
 export default mongoose;
